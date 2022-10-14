4fc69deabb57790efdfe7d3ab90cc0832562b4b5
eli <eliabdiel316@gmail.com>
2022-10-13 22:32:20 -0500

    ventanas de menu y inicio

--- Win32Project/Resource.h
+++ Win32Project/Resource.h
@@ -1,30 +1,41 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by Win32Project.rc
-
-#define IDS_APP_TITLE			103
-
-#define IDR_MAINFRAME			128
-#define IDD_WIN32PROJECT_DIALOG	102
-#define IDD_ABOUTBOX			103
-#define IDM_ABOUT				104
-#define IDM_EXIT				105
-#define IDI_WIN32PROJECT			107
-#define IDI_SMALL				108
-#define IDC_WIN32PROJECT			109
-#define IDC_MYICON				2
-#ifndef IDC_STATIC
-#define IDC_STATIC				-1
-#endif
-// Next default values for new objects
+// Archivo de inclusión generado de Microsoft Visual C++.
+// Usado por Win32Project.rc
 //
+#define IDC_MYICON                      2
+#define IDD_WIN32PROJECT_DIALOG         102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_WIN32PROJECT                107
+#define IDI_SMALL                       108
+#define IDC_WIN32PROJECT                109
+#define IDR_MAINFRAME                   128
+#define IDD_DIALO_RegistroN             130
+#define IDD_DIALOG1                     131
+#define IDD_DIALO_Menu                  131
+#define IDD_DIALO_INICIO                1001
+#define IDD_DIALO                       1002
+#define IDC_EDIT2                       1005
+#define IDC_EDIT3                       1006
+#define IDC_EDIT1                       1008
+#define IDC_EDIT4                       1009
+#define IDC_BUTTON1                     1010
+#define IDC_EDIT5                       1011
+#define IDC_BUTTON2                     1011
+#define IDC_BUTTON3                     1012
+#define IDC_BUTTON4                     1013
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-
-#define _APS_NO_MFC					130
-#define _APS_NEXT_RESOURCE_VALUE	129
-#define _APS_NEXT_COMMAND_VALUE		32771
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		110
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1012
+#define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
