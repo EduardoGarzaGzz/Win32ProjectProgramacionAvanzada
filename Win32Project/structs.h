25eeefda996c6b6418c3d3d856bdced8a3568420
Eduardo Uriel Garza Gonzalez <eduardo.garza@generalsoftcommx.onmicrosoft.com>
2022-10-15 03:14:12 -0500

    Se terminaron de agregar validaciones

--- Win32Project/structs.h
+++ Win32Project/structs.h
@@ -32,6 +32,56 @@ struct Usuario
 	struct Usuario* UltimoPuntero;
 } * ptr_usuarios;
 
+struct Cliente
+{
+	bool EsPremium;
+	int ComercioId;
+	char Nombre[50];
+	char Alias[15];
+	char Telefono[10];
+	char Email[25];
+	char Fecha[10];
+
+	struct Cliente* SiguentePuntero;
+	struct Cliente* UltimoPuntero;
+} * ptr_clientes;
+
+
+struct Promocion
+{
+	bool Estatus;
+	char Nombre[50];
+	float MontoPromocion;
+	int PorcentajeDescuento;
+	char Fecha[10];
+
+	struct Promocion* SiguentePuntero;
+	struct Promocion* UltimoPuntero;
+} * ptr_promociones;
+
+struct Consumo
+{
+	int ClienteId;
+	float Monto;
+	char Fecha[10];
+	float SubTotal;
+	float Descuento;
+	float Total;
+
+	struct Consumo* SiguentePuntero;
+	struct Consumo* UltimoPuntero;
+} * ptr_consumos;
+
+// Funciones para validad datos de las estructuras
+///////////////////////////////////////////////////
+
+bool es_caracter_especiales(char caracter)
+{
+	return (caracter >= 33 && caracter <= 47) ||
+		(caracter >= 58 && caracter <= 64) ||
+		(caracter >= 123 && caracter <= 126);
+}
+
 bool nombre_usuario_es_valido(char nombre_usuario[50])
 {
 	bool es_valido = true;
@@ -40,14 +90,14 @@ bool nombre_usuario_es_valido(char nombre_usuario[50])
 	// El espacio tiene el valor 32 en ASCII
 	// isalpha evalua que el caractes este entre el rango de valores ASCII para letras
 	// Para mayusculas son del 65 al 90 y para minusculas son del 97 al 122
-	for (int i = 0; i < longitud_texto; i++) 
+	for (int i = 0; i < longitud_texto; i++)
 		if (nombre_usuario[i] != 32 || !isalpha(nombre_usuario[i]))
 		{
 			es_valido = false;
 			break;
 		}
 
-	return es_valido && (longitud_texto >= 5 && longitud_texto <= 50);
+	return es_valido && longitud_texto >= 5 && longitud_texto <= 50;
 }
 
 bool alias_usuario_es_valido(char alias_usuario[10])
@@ -66,117 +116,124 @@ bool alias_usuario_es_valido(char alias_usuario[10])
 			break;
 		}
 
-	return es_valido && (longitud_texto >= 3 && longitud_texto <= 10);
+	return es_valido && longitud_texto >= 3 && longitud_texto <= 10;
 }
 
-// Contraseña
-// TODO Debe contener una letra, un numero y un caracter especial obligatoriamente
 bool contrasena_usuario_es_valida(char password[30])
 {
-	//bool es_valido = true;
 	const int longitud_texto = static_cast<int>(strlen(password));
 	bool tiene_letras = false;
 	bool tiene_nums = false;
 	bool tiene_caracteres_especiales = false;
-	char chr;
 
 	for (int i = 0; i < longitud_texto; i++)
 	{
-		chr = password[i];
-		if (isalpha(chr)) tiene_letras = true;
-		else if (isdigit(chr)) tiene_nums = true;
-		else if (true) tiene_caracteres_especiales = true;
+		if (isalpha(password[i])) tiene_letras = true;
+		else if (isdigit(password[i])) tiene_nums = true;
+		else if (es_caracter_especiales(password[i])) tiene_caracteres_especiales = true;
 	}
 
-	return (tiene_letras && tiene_nums && tiene_caracteres_especiales) && (longitud_texto >= 3 && longitud_texto <= 10);
+	return (tiene_letras && tiene_nums && tiene_caracteres_especiales) && longitud_texto >= 3 && longitud_texto <= 10;
 }
 
-// Cientes
-// Nombre
-// TODO Solo caracteres alfanumericos y espacios
-// TODO Minimo 5 maximo 50
-// Alias
-// TODO Solo caracteres alfanumericos y espacios
-// TODO Minimo 5 maximo 15
-// Telefono
-// TODO Solo caracteres numericos
-// TODO Exactamente 10
-// Email
-// TODO Validar el formato de email
-// TODO Minimo 5 maximo 25
-// TODO Agregar comercio
-// TODO Agregar fecha
-
-struct Cliente
+bool nombre_cliente_es_valido(char nombre[50])
 {
-	bool EsPremium;
-	char Nombre[200];
-	char Direccion[255];
+	bool es_valido = true;
+	const int longitud_texto = static_cast<int>(strlen(nombre));
 
-	struct Cliente* SiguentePuntero;
-	struct Cliente* UltimoPuntero;
-} * NodoActualDeCliente;
+	for (int i = 0; i < longitud_texto; i++)
+		if (nombre[i] != 32 || !isalpha(nombre[i]))
+		{
+			es_valido = false;
+			break;
+		}
 
-struct Cajero
+	return es_valido && longitud_texto >= 5 && longitud_texto <= 50;
+}
+
+bool alias_cliente_es_valido(char alias[15])
 {
-	bool EstaActivo;
-	char identidad[100];
-	char direccion[255];
-	float DineroDisponible;
-	float DineroTotal;
+	bool es_valido = true;
+	const int longitud_texto = static_cast<int>(strlen(alias));
 
-	struct Cajero* SiguentePuntero;
-	struct Cajero* UltimoPuntero;
-} * NodoActualDeCajero;
+	for (int i = 0; i < longitud_texto; i++)
+		if (alias[i] != 32 || !isalpha(alias[i]))
+		{
+			es_valido = false;
+			break;
+		}
 
-// Promocion
-// Nombre
-// TODO Solo caracteres alfanumericos y espacios
-// TODO Minimo 5 maximo 50
-// Monto para promociones
-// TODO Agregar Monto para promociones
-// TODO Solo valores numericos no negativos y mayores a 100
-// Porcentaje de descuento
-// TODO Agregar procentaje de descuento es decimal
-// TODO Valor numerico, mayor a 0 y menor a 100
-// Estatus
-// TODO Agregar estatus si esta activa o inactiva
-// TODO Agregar fecha
+	return es_valido && longitud_texto >= 5 && longitud_texto <= 15;
+}
 
-struct Promocion
+bool telefono_cliente_es_valido(char telefono[10])
 {
-	bool EstaActivo;
-	char Nombre[100];
-	char Descripcion[255];
-	float bono;
+	bool es_valido = true;
+	const int longitud_texto = static_cast<int>(strlen(telefono));
 
-	struct Promocion* SiguentePuntero;
-	struct Promocion* UltimoPuntero;
-} * NodoActualDePromocion;
+	for (int i = 0; i < longitud_texto; i++)
+		if (!isdigit(telefono[i]))
+		{
+			es_valido = false;
+			break;
+		}
 
-// Consumos
-// Cliente
-// TODO Agregar campo para seleccionar al cliente
-// Monto
-// TODO Solo numeros mayor a 0
-// TODO Agregar fecha
-// Subtotal
-// TODO Calcular automaticamente
-// Descuento
-// TODO Se asigna automaticamente si aplica
-// Total
-// TODO Automatico
+	return es_valido && longitud_texto == 10;
+}
 
-
-struct Consumo
+bool email_cliente_es_valido(char email[25])
 {
-	char NombreDelCliente[100];
-	float Monto;
-	float SubTotal;
-	float Descuento;
-	float Total;
-	char Dato[20];
+	const int longitud_texto = static_cast<int>(strlen(email));
+	bool tiene_nombre_cuenta = false;
+	bool tiene_arroba = false;
+	bool tiene_nombre_dominio = false;
+	bool tiene_punto = false;
+	bool tiene_extencion = false;
 
-	struct Consumo* SiguentePuntero;
-	struct Consumo* UltimoPuntero;
-} * NodoActualDeConsumo;
+	for (int i = 0; i < longitud_texto; i++)
+	{
+		if (isalpha(email[i])) tiene_nombre_cuenta = true;
+		if (tiene_nombre_cuenta && email[i] == 64) tiene_arroba = true; // 64 es el @
+		if (tiene_arroba && isalpha(email[i])) tiene_nombre_dominio = true;
+		if (tiene_nombre_dominio && email[i] == 46) tiene_punto = true; // 46 es el .
+		if (tiene_punto && isalpha(email[i])) tiene_extencion = true;
+	}
+
+	return tiene_nombre_cuenta
+		&& tiene_arroba
+		&& tiene_nombre_dominio
+		&& tiene_punto
+		&& tiene_extencion
+		&& longitud_texto >= 5
+		&& longitud_texto <= 25;
+}
+
+bool nombre_promocion_es_valido(char nombre[50])
+{
+	bool es_valido = true;
+	const int longitud_texto = static_cast<int>(strlen(nombre));
+
+	for (int i = 0; i < longitud_texto; i++)
+		if (nombre[i] != 32 || !isalpha(nombre[i]))
+		{
+			es_valido = false;
+			break;
+		}
+
+	return es_valido && longitud_texto >= 5 && longitud_texto <= 50;
+}
+
+bool monto_promociones_es_valida(float monto_promociones)
+{
+	return monto_promociones >= 0 && monto_promociones <= 100;
+}
+
+bool porcentaje_descuento_promocion_es_valido(int porcentaje_descuneto)
+{
+	return porcentaje_descuneto >= 0 && porcentaje_descuneto <= 100;
+}
+
+bool monto_consumo_es_valido(float monto)
+{
+	return monto > 0;
+}
