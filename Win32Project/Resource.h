8fb14bd238b43f8c8bcc21c9f9ab8971dff0cdeb
eli <eliabdiel316@gmail.com>
2022-10-14 19:13:42 -0500

    parte de las ventanas

--- Win32Project/Resource.h
+++ Win32Project/Resource.h
@@ -13,19 +13,53 @@
 #define IDC_WIN32PROJECT                109
 #define IDR_MAINFRAME                   128
 #define IDD_DIALO_RegistroN             130
+#define IDD_DIALO_RegistroNuevoAdmin    130
 #define IDD_DIALOG1                     131
 #define IDD_DIALO_Menu                  131
+#define IDD_DIALO_MenuPrincipal         131
+#define IDD_DIALO_OpcionesUsuario       132
+#define IDD_DIALO_MostrarUsuario        133
+#define IDD_DIALO_RegistrarUsuario      134
+#define IDD_DIALO_ActualizacionUsuario  135
+#define IDD_DIALO_BorrarUsuario         136
+#define IDD_DIALO_OpcionesCliente       137
+#define IDD_DIALO_OpcionesCajero        138
+#define IDD_DIALO_OpcionesPromocion     139
+#define IDD_DIALO_OpcionesConsumo       140
+#define IDD_DIALO_RegistrarCliente      141
+#define IDD_DIALO_RegistrarCajero       142
+#define IDD_DIALO_RegistrarPromocion    143
+#define IDD_DIALO_RegistrarConsumo      144
+#define IDD_DIALO_BorrarCliente         145
+#define IDD_DIALO_BorrarCajero          146
+#define IDD_DIALO_BorrarPromocion       147
+#define IDD_DIALO_BorrarConsumo         148
+#define IDD_DIALO_MostrarCliente        149
+#define IDD_DIALO_MostrarCajero         150
+#define IDD_DIALO_MostrarPromocion      151
+#define IDD_DIALO_MostrarConsumo        152
+#define IDD_DIALO_ActualizacionCliente  153
+#define IDD_DIALO_ActualizacionCajero   154
+#define IDD_DIALO_ActualizacionPromocion 155
+#define IDD_DIALOG3                     156
+#define IDD_DIALO_ActualizacionConsumo  156
 #define IDD_DIALO_INICIO                1001
+#define IDD_DIALO_InicioDeSeccion       1001
 #define IDD_DIALO                       1002
 #define IDC_EDIT2                       1005
 #define IDC_EDIT3                       1006
+#define IDC_EDIT9                       1007
 #define IDC_EDIT1                       1008
 #define IDC_EDIT4                       1009
 #define IDC_BUTTON1                     1010
 #define IDC_EDIT5                       1011
 #define IDC_BUTTON2                     1011
 #define IDC_BUTTON3                     1012
+#define IDC_EDIT6                       1012
 #define IDC_BUTTON4                     1013
+#define IDC_EDIT7                       1013
+#define IDC_BUTTON5                     1014
+#define IDC_EDIT8                       1014
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -33,7 +67,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_RESOURCE_VALUE        157
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1012
 #define _APS_NEXT_SYMED_VALUE           110
