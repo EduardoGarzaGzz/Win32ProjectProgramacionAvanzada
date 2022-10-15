e6c70dd5012f42285fa849765b9e3b5dacd1c07e
Eduardo Uriel Garza Gonzalez <eduardo.garza@generalsoftcommx.onmicrosoft.com>
2022-10-14 22:04:54 -0500

    Se agregaron todos para las cosas faltantes de las estructuras

--- Win32Project/Win32Project.cpp
+++ Win32Project/Win32Project.cpp
@@ -3,29 +3,54 @@
 
 HINSTANCE hInst;
 
-LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK
+WndProc(HWND, UINT, WPARAM, LPARAM
+);
 
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
+int APIENTRY
+wWinMain(_In_
+HINSTANCE hInstance, _In_opt_
+HINSTANCE hPrevInstance, _In_
+LPWSTR lpCmdLine,
+        _In_
+int nCmdShow
+)
 {
-	hInst = hInstance;
-	DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALO_MenuPrincipal), nullptr, WndProc);
-	return 0;
+hInst = hInstance;
+DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALO_MenuPrincipal), nullptr, WndProc
+);
+return 0;
 }
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK
+WndProc(HWND
+hWnd,
+UINT message, WPARAM
+wParam,
+LPARAM lParam
+)
 {
-	switch (message)
-	{
-	case WM_COMMAND:
-		{
-			switch (LOWORD(wParam))
-			{
-			case IDM_EXIT:
-				DestroyWindow(hWnd);
-				break;
-			default:
-				return DefWindowProc(hWnd, message, wParam, lParam);
-			}
+switch (message)
+{
+case WM_INITDIALOG:
+{
+
+return 0;
+}
+case WM_COMMAND:
+{
+switch (
+LOWORD(wParam)
+)
+{
+case IDM_EXIT:
+DestroyWindow(hWnd);
+break;
+default:
+return
+DefWindowProc(hWnd, message, wParam, lParam
+);
+}
 		}
 		break;
 	case WM_DESTROY:
--- Win32Project/structs.h
+++ Win32Project/structs.h
@@ -1,21 +1,52 @@
 ﻿#pragma once
 
+// Usuario
+// Nombre completo del usuario
+// TODO Validar que solo sean caracteres y espacios
+// TODO minimo 5 caracteres maximo 50
+// Alias del usuario
+// TODO Solo caracteres alfanumericos
+// TODO Minimo 3 maximo 10
+// Foto del usuario
+// TODO Agregar campo para la imagen del usuario
+// TODO Agregar el comercio solo es uno para todos este se registrar con el admin
+// Contraseña
+// TODO minimo 3 maximo 10
+// TODO Debe contener una letra, un numero y un caracter especial obligatoriamente
+// TODO Agregar tipo de usuario
+
 struct Usuario {
     bool EsAdmin;
     char NombreDeUsuario[100];
     char Contasena[30];
 
-    struct Usuario* SiguentePuntero;
-    struct Usuario* UltimoPuntero;
+    struct Usuario *SiguentePuntero;
+    struct Usuario *UltimoPuntero;
 } *NodoActualDeUsuario;
+
+// Cientes
+// Nombre
+// TODO Solo caracteres alfanumericos y espacios
+// TODO Minimo 5 maximo 50
+// Alias
+// TODO Solo caracteres alfanumericos y espacios
+// TODO Minimo 5 maximo 15
+// Telefono
+// TODO Solo caracteres numericos
+// TODO Exactamente 10
+// Email
+// TODO Validar el formato de email
+// TODO Minimo 5 maximo 25
+// TODO Agregar comercio
+// TODO Agregar fecha
 
 struct Cliente {
     bool EsPremium;
     char Nombre[200];
-    char Dirección[255];
+    char Direccion[255];
 
-    struct Cliente* SiguentePuntero;
-    struct Cliente* UltimoPuntero;
+    struct Cliente *SiguentePuntero;
+    struct Cliente *UltimoPuntero;
 } *NodoActualDeCliente;
 
 struct Cajero {
@@ -25,9 +56,23 @@ struct Cajero {
     float DineroDisponible;
     float DineroTotal;
 
-    struct Cajero* SiguentePuntero;
-    struct Cajero* UltimoPuntero;
+    struct Cajero *SiguentePuntero;
+    struct Cajero *UltimoPuntero;
 } *NodoActualDeCajero;
+
+// Promocion
+// Nombre
+// TODO Solo caracteres alfanumericos y espacios
+// TODO Minimo 5 maximo 50
+// Monto para promociones
+// TODO Agregar Monto para promociones
+// TODO Solo valores numericos no negativos y mayores a 100
+// Porcentaje de descuento
+// TODO Agregar procentaje de descuento es decimal
+// TODO Valor numerico, mayor a 0 y menor a 100
+// Estatus
+// TODO Agregar estatus si esta activa o inactiva
+// TODO Agregar fecha
 
 struct Promocion {
     bool EstaActivo;
@@ -35,9 +80,23 @@ struct Promocion {
     char Descripcion[255];
     float bono;
 
-    struct Promocion* SiguentePuntero;
-    struct Promocion* UltimoPuntero;
+    struct Promocion *SiguentePuntero;
+    struct Promocion *UltimoPuntero;
 } *NodoActualDePromocion;
+
+// Consumos
+// Cliente
+// TODO Agregar campo para seleccionar al cliente
+// Monto
+// TODO Solo numeros mayor a 0
+// TODO Agregar fecha
+// Subtotal
+// TODO Calcular automaticamente
+// Descuento
+// TODO Se asigna automaticamente si aplica
+// Total
+// TODO Automatico
+
 
 struct Consumo {
     char NombreDelCliente[100];
