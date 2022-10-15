4e04ce86f22612d659e984fcbe8a9e3a22e8ba76
Eduardo Uriel Garza Gonzalez <eduardo.garza@generalsoftcommx.onmicrosoft.com>
2022-10-14 20:52:51 -0500

    Se limpio el proyecto y se restructuro a una nueva estructura de carpetas

--- Win32Project/Resource.h
+++ Win32Project/Resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
-// Archivo de inclusión generado de Microsoft Visual C++.
-// Usado por Win32Project.rc
+// Microsoft Visual C++ generated include file.
+// Used by Win32Project.rc
 //
 #define IDC_MYICON                      2
 #define IDD_WIN32PROJECT_DIALOG         102
@@ -56,10 +56,13 @@
 #define IDC_BUTTON2                     1011
 #define IDC_BUTTON3                     1012
 #define IDC_EDIT6                       1012
+#define IDC_LIST1                       1012
 #define IDC_BUTTON4                     1013
 #define IDC_EDIT7                       1013
+#define IDC_LIST2                       1013
 #define IDC_BUTTON5                     1014
 #define IDC_EDIT8                       1014
+#define IDC_LIST3                       1015
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -69,7 +72,7 @@
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        157
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1012
+#define _APS_NEXT_CONTROL_VALUE         1016
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
---
+++ Win32Project/structs.h
@@ -0,0 +1,52 @@
+﻿#pragma once
+
+struct Usuario {
+    bool EsAdmin;
+    char NombreDeUsuario[100];
+    char Contasena[30];
+
+    struct Usuario* SiguentePuntero;
+    struct Usuario* UltimoPuntero;
+} *NodoActualDeUsuario;
+
+struct Cliente {
+    bool EsPremium;
+    char Nombre[200];
+    char Dirección[255];
+
+    struct Cliente* SiguentePuntero;
+    struct Cliente* UltimoPuntero;
+} *NodoActualDeCliente;
+
+struct Cajero {
+    bool EstaActivo;
+    char identidad[100];
+    char direccion[255];
+    float DineroDisponible;
+    float DineroTotal;
+
+    struct Cajero* SiguentePuntero;
+    struct Cajero* UltimoPuntero;
+} *NodoActualDeCajero;
+
+struct Promocion {
+    bool EstaActivo;
+    char Nombre[100];
+    char Descripcion[255];
+    float bono;
+
+    struct Promocion* SiguentePuntero;
+    struct Promocion* UltimoPuntero;
+} *NodoActualDePromocion;
+
+struct Consumo {
+    char NombreDelCliente[100];
+    float Monto;
+    float SubTotal;
+    float Descuento;
+    float Total;
+    char Dato[20];
+
+    struct Consumo* SiguentePuntero;
+    struct Consumo* UltimoPuntero;
+} *NodoActualDeConsumo;
